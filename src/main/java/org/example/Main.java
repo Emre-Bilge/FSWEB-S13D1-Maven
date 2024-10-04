f5d7bb5d46107ea4d7d8b1be6a97f5ad1a67fc82
Dogancan Kınık <dogancankinik@gmail.com>
2024-10-04 12:11:07 +0300

    Update ResultAnalyzer.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,25 +3,47 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+
+        if(clock <0 || clock > 23){
+            return false;
+        }
+        return isBarking && (clock < 8 || clock >= 20);
+
     }
 
+
+
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return firstAge >= 13 && firstAge <= 19 || secondAge >= 13 && secondAge <= 19 || thirdAge >= 13 && thirdAge <= 19;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     if(isSummer && temp >= 25 && temp <= 45){
+         return true ;
+     }else return !isSummer && temp >= 25 && temp <= 35;
+
     }
 
     public static double area(double width, double height) {
-       
+       if(width < 0 || height < 0){
+           System.out.println("entered numbers cannot be negative");
+           return -1;
+       }else{
+           return width*height;
+       }
     }
 
     public static double area(double radius) {
-       
+       if(radius < 0 ){
+           return -1;
+       }else{
+           return radius*radius*Math.PI;
+       }
+
     }
+
 }
